f52af13be61229c9deb8754d4ff7458562a32ba8
Azzary <remicolin2707@gmail.com>
2023-04-19 16:46:14 +1100

    add entre event on search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -2,9 +2,21 @@ const rechercher = document.getElementById("rechercher");
 const btnRechercher = document.getElementById("btnRechercher");
 
 
+// add event on entre key
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        doSearch();
+    }
+});
 
 btnRechercher.addEventListener("click", () => {
+    doSearch();
+});
+
+
+function doSearch() {
     var keycookie = rechercher.value;
     document.cookie = 'rechercher=' + keycookie;
-    document.location.href="/";
-});
+    document.location.href = "/";
+}
